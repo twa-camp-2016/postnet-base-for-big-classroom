f1d86e09e98cff6c10aa6558c67785d58f154edb
ww <yanruoxinb@github.com>
2016-08-01 23:14:13 +0800

    wangting Bar_to_zip_action

--- Actions/BarcodeToZIPAction.js
+++ Actions/BarcodeToZIPAction.js
@@ -1,3 +1,24 @@
 /**
  * Created by wt on 16-8-1.
  */
+/*global require,module*/
+const changeToZIP = require('../src/BarcodeToZIP');
+const Zip = new changeToZIP();
+class BarcodeToZIPAction {
+    constructor() {
+        this.name = 'BarcodeToZIP';
+        this.help = 'Please input Barcode:'.trim();
+    }
+
+    doAction(cmd) {
+        if (cmd === 'q') {
+            return 'init';
+        }
+        else {
+            console.log('I\'m converting the barcode: ' + cmd);
+            console.log(Zip.changeToZIP(cmd));
+            return 'BarcodeToZIP'
+        }
+    }
+}
+module.exports = BarcodeToZIPAction;
