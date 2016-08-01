5b01c8115a0ef9a5589d6de07d3d9bab55e8560f
ww <yanruoxinb@github.com>
2016-08-01 23:14:36 +0800

    wangting init action

--- Actions/InitAction.js
+++ Actions/InitAction.js
@@ -1,3 +1,31 @@
 /**
  * Created by wt on 16-8-1.
  */
+class InitAction {
+    constructor() {
+        this.name = 'init';
+        this.help = `
+    ++++++++++++++++++++    Welcome     ++++++++++++++++++++
+                    1 - ZIPToBarcode
+                    2 - barcodeToZIP
+                    q - quit`.trim();
+    }
+
+    doAction(cmd) {
+        switch (cmd) {
+            case '1':
+                return 'ZIPToBarcode';
+                break;
+            case '2':
+                return 'BarcodeToZIP';
+                break;
+            case 'q':
+                process.exit();
+                break;
+            default:
+                console.log("input error,input again");
+                break;
+        }
+    }
+}
+module.exports = InitAction;
