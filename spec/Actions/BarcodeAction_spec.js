785a6a3ae21d13b4262a4346da8f328b19958930
Demo-tong <18829292377@163.com>
2016-08-02 01:17:53 +0800

    tongyang add BarcodeAction_spec

--- spec/Actions/BarcodeAction_spec.js
+++ spec/Actions/BarcodeAction_spec.js
@@ -4,24 +4,24 @@
 'use strict';
 const BarcodeAction = require('../../transformer/actions/BarcodeAction');
 
-fdescribe("BarcodeAction", function(){
-let barcodeAction = new BarcodeAction();
+fdescribe("BarcodeAction", function () {
+  let barcodeAction = new BarcodeAction();
 
-  it("input '1' ecpect 'input postcode'", function(){
+  it("input '1' ecpect 'input postcode'", function () {
     let input = '1';
     let expected = 'input postcode';
 
     expect(barcodeAction.doAction(input)).toEqual(expected);
   });
 
-  it("input '2' expect 'init", function(){
+  it("input '2' expect 'init", function () {
     let input = '2';
     let expected = 'init';
 
     expect(barcodeAction.doAction(input)).toEqual(expected);
   });
 
-  it("input 'q' ecpect exit", function(){
+  it("input 'q' ecpect exit", function () {
     spyOn(process, 'exit');
     let input = 'q';
 
@@ -29,7 +29,7 @@ let barcodeAction = new BarcodeAction();
     expect(process.exit).toHaveBeenCalledWith(0);
   });
 
-  it("input other letter", function(){
+  it("input other letter", function () {
     spyOn(console, 'log');
     let input = 'e';
     let expected = '无效的输入';
