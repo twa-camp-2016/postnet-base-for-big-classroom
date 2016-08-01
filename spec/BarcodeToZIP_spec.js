a3d66536ade8e5076899db15cb1c201ff949faad
ww <yanruoxinb@github.com>
2016-08-01 23:17:52 +0800

    wangting Bar_to_zip_spec

--- spec/BarcodeToZIP_spec.js
+++ spec/BarcodeToZIP_spec.js
@@ -3,25 +3,35 @@
  */
 /* global require,describe,it,toEqual,expect,toBeUndefined*/
 const BarcodeToZIP = require("../src/BarcodeToZIP.js");
-describe("BarcodeToZIP", function () {
+fdescribe("BarcodeToZIP", function () {
+    let zip;
+    beforeEach(()=> {
+        zip = new BarcodeToZIP();
+    });
     it("should return Barcode to ZIP", function () {
         let input = '| ::|:| ::||: |:::| :||:: ::|:| ||::: |';
-        let result = '23762';
-        let temp = BarcodeToZIP.changeToZIP(input);
-        expect(temp).toEqual(result);
+        let temp = zip.changeToZIP(input);
+        expect(temp).toEqual({
+            error: undefined,
+            data: '23762'
+        });
     });
 
     it("input error", function () {
         let input = '| ::|:| ::||: |:::| :||:: ::|:||';
-        let result = undefined;
-        let temp = BarcodeToZIP.changeToZIP(input);
-        expect(temp).toEqual(result);
+        let temp = zip.changeToZIP(input);
+        expect(temp).toEqual({
+            data: undefined,
+            error: 'input error'
+        });
     });
 
     it("wrong checkDigit", function () {
-        let input = '| ::|:| ::||: |:::| :||:: :|:|: |';
-        let result = undefined;
-        let temp = BarcodeToZIP.changeToZIP(input);
-        expect(temp).toEqual(result);
+        let input = '| ::|:| ::||: |:::| :||:: :|:|: :|:|: |';
+        let temp = zip.changeToZIP(input);
+        expect(temp).toEqual({
+            data: undefined,
+            error: 'invalid check digit'
+        });
     });
 });
