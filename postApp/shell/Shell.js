139267ce592cd28523624e780ca9e5d4f584dfc4
wangqi <774948439@qq.com>
2016-08-02 01:21:22 +0000

    shell

--- postApp/shell/Shell.js
+++ postApp/shell/Shell.js
@@ -1,3 +1,22 @@
 /**
  * Created by wangqi on 16-8-1.
  */
+'use strict';
+class Shell {
+    constructor(router, repl) {
+        this.router = router;
+        this.repl = repl;
+    }
+    start() {
+        this.router.start();
+        this.repl.start({
+            prompt: '->', eval: (cmd, context, filename, callback) => {
+                this.router.handle(cmd.trim());
+                this.router.start();
+                callback();
+            }
+        });
+    }
+}
+
+module.exports = Shell;
