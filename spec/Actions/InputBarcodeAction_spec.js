4667a6cc83fcdeac1efda33b6a777b682b99b6cd
Demo-tong <18829292377@163.com>
2016-08-02 10:15:31 +0800

    tongyang add InputBarcodeAction_spec

---
+++ spec/Actions/InputBarcodeAction_spec.js
@@ -0,0 +1,41 @@
+/**
+ * Created by tong on 16-8-2.
+ */
+'use strict';
+
+const InputBarcodeAction = require('./../../transformer/actions/InputBarcodeAction');
+
+fdescribe("InputBarcodeAction", function () {
+  let inputBarcodeAction = new InputBarcodeAction();
+
+  it("input 'q' to exit", function () {
+    let input = 'q';
+    let expected = 'barcodeToPostcode';
+
+    expect(inputBarcodeAction.doAction(input)).toEqual(expected);
+  });
+
+  it("input the barcode and return the correct result", function () {
+    spyOn(console, 'log');
+
+    let input = '| :::|| ::|:| ::||: :|::| :|:|: :|:|: |';
+    let expected = 'input barcode';
+    let expectString = '12345';
+
+    expect(inputBarcodeAction.doAction(input)).toEqual(expected);
+    expect(console.log).toHaveBeenCalledWith('The result is:');
+    expect(console.log).toHaveBeenCalledWith(expectString);
+  });
+
+  it("input the barcode and return error info", function () {
+    spyOn(console, 'log');
+
+    let input = '| :::*| ::|:| ::||: :|::| :|:|: :|:|: |';
+    let expected = 'input barcode';
+    let expectString = "error input(only '|'':'' 'can be accepted and ' 'is must)";
+
+    expect(inputBarcodeAction.doAction(input)).toEqual(expected);
+    expect(console.log).toHaveBeenCalledWith('The result is:');
+    expect(console.log).toHaveBeenCalledWith(expectString);
+  });
+});
