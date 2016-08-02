69c40043bbedf4b562f90207b93fb779dc19d96c
Demo-tong <18829292377@163.com>
2016-08-02 11:31:22 +0800

    tongyang change instance result to Object

--- spec/transformer/postcodeToBarcode_spec.js
+++ spec/transformer/postcodeToBarcode_spec.js
@@ -7,60 +7,80 @@ const postcodeToBarcode = require("../../transformer/core/PostcodeToBarcode.js")
 //postcode to barcode
 describe("transferToBarcode", function () {
   let theCall;
-  
+
   beforeEach(()=> {
     theCall = new postcodeToBarcode();
   });
 
   it("postCode transformer to barcode", function () {
     let input = "12345-1234";
-    let expected = "|:::||::|:|::||::|::|:|:|::::||::|:|::||::|::|:|:|:|";
     let check = 5;
+    let expected = {
+      error: '',
+      data: "|:::||::|:|::||::|::|:|:|::::||::|:|::||::|::|:|:|:|\n" + "cd is " + check
+    };
 
-    expect(theCall.transferToBarcode(input)).toEqual(expected + '\ncd is ' + check);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("postCode transformer to barcode", function () {
     let input = "123451234";
-    let expected = "|:::||::|:|::||::|::|:|:|::::||::|:|::||::|::|:|:|:|";
-    let check = 5;
+    let expected = {
+      error: '',
+      data: "|:::||::|:|::||::|::|:|:|::::||::|:|::||::|::|:|:|:|\n" + "cd is 5"
+    };
 
-    expect(theCall.transferToBarcode(input)).toEqual(expected + '\ncd is ' + check);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("postCode transformer to barcode", function () {
     let input = "12345";
-    let expected = "|:::||::|:|::||::|::|:|:|::|:|:|";
     let check = 5;
+    let expected = {
+      error: '',
+      data: "|:::||::|:|::||::|::|:|:|::|:|:|\n" + "cd is " + check
+    };
 
-    expect(theCall.transferToBarcode(input)).toEqual(expected + '\ncd is ' + check);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("check the number format_length", function () {
     let input = "1234";
-    let expected = "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')";
+    let expected = {
+      error: "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')",
+      data: ''
+    };
 
-    expect(theCall.transferToBarcode(input)).toBe(expected);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("check the number format_letter", function () {
     let input = "1234a1234";
-    let expected = "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')";
+    let expected = {
+      error: "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')",
+      data: ''
+    };
 
-    expect(theCall.transferToBarcode(input)).toBe(expected);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("check the number format_position", function () {
     let input = "123-41234";
-    let expected = "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')";
+    let expected = {
+      error: "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')",
+      data: ''
+    };
 
-    expect(theCall.transferToBarcode(input)).toBe(expected);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 
   it("check the number format_position", function () {
     let input = "";
-    let expected = "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')";
+    let expected = {
+      error: "the letter or the length of number is illegal(the length should be 5/9/10 contain'-')",
+      data: ''
+    };
 
-    expect(theCall.transferToBarcode(input)).toBe(expected);
+    expect(theCall.transferToBarcode(input)).toEqual(expected);
   });
 });
