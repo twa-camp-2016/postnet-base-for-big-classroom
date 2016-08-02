11c21250277f21932e6da8320e099dd8f7099b6d
wangqi <774948439@qq.com>
2016-08-02 01:15:43 +0000

    barcodeAction

--- postApp/actions/BarcodeAction.js
+++ postApp/actions/BarcodeAction.js
@@ -1,3 +1,30 @@
 /**
  * Created by wangqi on 16-8-1.
  */
+'use strict';
+const Barcode = require('../core/Barcode');
+const barcodes = new Barcode();
+class BarcodeAction {
+    constructor() {
+        this.name = 'barcode';
+        this.help = '请选择：输入条形码或者q退出或者r返回上一层:';
+    }
+
+    doAction(cmd) {
+        if (cmd == 'q') {
+            process.exit(0);
+
+        }
+        else if (cmd == 'r') {
+            return 'init';
+
+        }
+        else {
+            const zipcode = barcodes.transformToZipCode(cmd);
+            console.log(zipcode.value);
+            return 'barcode'
+        }
+
+    }
+}
+module.exports = BarcodeAction;
