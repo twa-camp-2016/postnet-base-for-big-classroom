fdd3e0f6b416d743df34712a48673760fb574ff7
wangqi <774948439@qq.com>
2016-08-02 01:16:31 +0000

    initAction

--- postApp/actions/InitAction.js
+++ postApp/actions/InitAction.js
@@ -1,3 +1,31 @@
 /**
  * Created by wangqi on 16-8-1.
  */
+
+'use strict';
+const postAppCore = require('../core/Barcode');
+class InitAction {
+    constructor() {
+        this.name = 'init';
+        this.help = '请选择功能:1.邮编转编码 2.编码转邮编 3.退出';
+    }
+
+    doAction(cmd) {
+        switch (cmd) {
+            case'1':
+                return 'zipcode';
+            case'2':
+                return 'barcode';
+            case'3':
+                console.log('bye');
+                // replServer.close();
+                process.exit(0);
+            default:
+                console.log('error');
+                return 'init';
+
+        }
+    }
+}
+
+module.exports = InitAction;
