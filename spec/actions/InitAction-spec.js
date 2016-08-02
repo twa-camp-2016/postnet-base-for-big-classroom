ad736787410c6cbdb69d1914569ae8f19c368cc7
wangqi <774948439@qq.com>
2016-08-02 01:22:45 +0000

    init

--- spec/actions/InitAction-spec.js
+++ spec/actions/InitAction-spec.js
@@ -1,4 +1,35 @@
-
 /**
  * Created by wangqi on 16-8-1.
  */
+'use strict';
+const InitAction = require('../../postApp/actions/InitAction');
+describe("init", ()=> {
+    let initAction = new InitAction();
+    it("should return zipcode", function () {
+        let cmd = "1";
+        let result = initAction.doAction(cmd);
+        let expectResult = "zipcode";
+        expect(result).toEqual(expectResult);
+    });
+    it("should return barcode", ()=> {
+        let cmd = "2";
+        let result = initAction.doAction(cmd);
+        let expectResult = "barcode";
+        expect(result).toEqual(expectResult);
+    });
+    it("should to end", ()=> {
+        let cmd = "3";
+        spyOn(console, 'log');
+        spyOn(process, 'exit');
+        let result = initAction.doAction(cmd);
+        let expectResult = 0;
+        expect(process.exit).toHaveBeenCalledWith(expectResult);
+    });
+    it("should return init", ()=> {
+        let cmd = "5";
+        spyOn(console, 'log');
+        let result = initAction.doAction(cmd);
+        let expectResult = "init";
+        expect(result).toEqual(expectResult);
+    });
+});
